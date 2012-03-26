288a06c1c793f98cf393df9b28c16aa01370f5aa
Luis Correia <buga@loide.net>
2012-03-26 22:00:04 +0100

    desactivar modulos FUC e DOCENTE\nmodificar o script de sync Linux para copiar apenas sumários

--- syncmoodle.sh
+++ syncmoodle.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-sudo cp -Rv /home/lfcorreia/devel/moodle/* /var/www/moodle/web/mod
+#sudo cp -Rv /home/lfcorreia/devel/moodle/fuc/ /var/www/moodle/web/mod
+#sudo cp -Rv /home/lfcorreia/devel/moodle/docente/ /var/www/moodle/web/mod
+sudo cp -Rv /home/lfcorreia/devel/moodle/sumarios/ /var/www/moodle/web/mod
+
