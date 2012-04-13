767fe8a57c26607200572f46e9df3c5680e3cc37
Luis Correia <buga@loide.net>
2012-04-13 21:36:24 +0100

    sumarios finalmente funcionam! o editor passou a ser textarea. falta evitar mais do que uma instancia, e mais umas coisas.

--- syncmoodle.sh
+++ syncmoodle.sh
@@ -3,4 +3,5 @@
 #sudo cp -Rv /home/lfcorreia/devel/moodle/fuc/ /var/www/moodle/web/mod
 #sudo cp -Rv /home/lfcorreia/devel/moodle/docente/ /var/www/moodle/web/mod
 sudo cp -Rv /home/lfcorreia/devel/moodle/sumarios/ /var/www/moodle/web/mod
+#sudo cp -Rv /home/lfcorreia/devel/moodle/book/ /var/www/moodle/web/mod
 
