66a5378ec3d0a6b9d882fa75ed4c4ecbc7f89324
Paulo Dias <skydivingpt@gmail.com>
2012-04-18 22:36:26 +0100

    FDOC - Ficha docente dir

---
+++ fdoc/lang/en_utf8/fdoc.php
@@ -0,0 +1,12 @@
+<?php
+
+$string['fuc'] = 'fuc';
+
+$string['modulename'] = 'fuc';
+$string['modulenameplural'] = 'fucS';
+
+$string['fucfieldset'] = 'Custom example fieldset';
+$string['fucintro'] = 'fuc Intro';
+$string['fucname'] = 'fuc Name';
+
+?>
---
+++ fdoc/lang/pt__utf8/fdoc.php
@@ -0,0 +1,12 @@
+<?php
+
+$string['FDoc'] = 'FDoc';
+
+$string['modulename'] = 'FDoc';
+$string['modulenameplural'] = 'FDocS';
+
+$string['FDocfieldset'] = 'Custom example fieldset';
+$string['FDocintro'] = 'FDoc Intro';
+$string['FDocname'] = 'FDoc Name';
+
+?>
