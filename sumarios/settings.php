45f9b3292c4caa59800ff1f21de6134895742e6b
Jose Proenca de Andrade <jose.p.andrade@gmail.com>
2012-05-24 22:27:35 +0100

    Added the begining of other config sets.

--- sumarios/settings.php
+++ sumarios/settings.php
@@ -5,14 +5,20 @@ defined('MOODLE_INTERNAL') || die;
 if ($ADMIN->fulltree) {
     require_once($CFG->dirroot.'/mod/sumarios/lib.php');
 
-    if (!isset($CFG->sumarios_tipobd)) {
+    if (!isset($CFG->sumarios_db_type)) {
+		$CFG->sumarios_db_type = SUMARIOS_MYSQL;
+		$CFG->sumarios_db_server = "";
+		$CFG->sumarios_db_user = "";
+		$CFG->sumarios_db_pass = "";
+		$CFG->sumarios_db_table = "";
     }
 
     $options = array(SUMARIOS_MYSQL => 'Oracle MySQL',
                      SUMARIOS_MSSQL => 'Microsoft SQL Server');
                      
-    $settings->add(new admin_setting_configselect('sumarios_tipobd', get_string('sumariosdatabase', 'sumarios'),
+    $settings->add(new admin_setting_configselect('sumarios_db_type', get_string('sumariosdatabase', 'sumarios'),
                        get_string('sumariosdatabase', 'sumarios'), SUMARIOS_MYSQL, $options));
                        
+   $settings->add(new admin_setting_configtext_trim_lower($name, $visiblename, $description, $defaultsetting))
 
 }
