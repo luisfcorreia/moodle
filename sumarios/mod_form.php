21614f40b474213c0275c69b6cccd077a0cfbf1a
Jose Proenca de Andrade <jose.p.andrade@gmail.com>
2012-05-26 18:37:44 +0100

    Change editor to Tyni MCE.

--- sumarios/mod_form.php
+++ sumarios/mod_form.php
@@ -60,10 +60,14 @@ class mod_sumarios_mod_form extends moodleform_mod {
         $mform->addHelpButton('name', 'sumariosname', 'sumarios');
 
         //-------------------------------------------------------------------------------
-        $mform->addElement('textarea', 'texto', get_string('sumariostexto', 'sumarios'),array('cols'=>48, 'rows'=>6));
-        $mform->setType('texto', PARAM_TEXT);
-        $mform->addRule('texto', get_string('required'), 'required', null, 'client');
+//        $mform->addElement('textarea', 'texto', get_string('sumariostexto', 'sumarios'),array('cols'=>48, 'rows'=>6));
+//        $mform->setType('texto', PARAM_TEXT);
+//        $mform->addRule('texto', get_string('required'), 'required', null, 'client');
 
+        $mform->addElement('editor', 'texto', get_string('sumariostexto', 'sumarios'));
+        // $mform->addHelpButton('texto', 'sumariostexto');
+        $mform->setType('texto', PARAM_RAW);
+        
         //-------------------------------------------------------------------------------
         $mform->addElement('date_selector', 'timeclass', get_string('sumariosdata','sumarios'));
         $mform->addRule('timeclass', get_string('required'), 'required', null, 'client');
