1d905b2d327595134b99753d3b1019e77274d6c6
Luis Correia <buga@loide.net>
2012-05-27 17:22:18 +0100

    refactor look

--- sumarios/settings.php
+++ sumarios/settings.php
@@ -6,32 +6,55 @@ if ($ADMIN->fulltree) {
     require_once($CFG->dirroot.'/mod/sumarios/lib.php');
 
     if (!isset($CFG->sumarios_db_type)) {
-		$CFG->sumarios_db_type = SUMARIOS_MYSQL;
-		$CFG->sumarios_db_server = "";
-		$CFG->sumarios_db_user = "";
-		$CFG->sumarios_db_pass = "";
-		$CFG->sumarios_db_table = "";
-		$module->cron=0;
+			$CFG->sumarios_db_type = "mysqli";
+			$CFG->sumarios_db_server = "";
+			$CFG->sumarios_db_user = "";
+			$CFG->sumarios_db_pass = "";
+			$CFG->sumarios_db_table = "";
+			$CFG->sumarios_cron = 0;
+			$module->cron=0;
     }
 
     $options = sumarios_get_database_available();                     
                      
-    $settings->add(new admin_setting_configselect('sumarios_db_type', get_string('sumarios_db_type', 'sumarios'),
-                       get_string('sumarios_db_type_text', 'sumarios'), SUMARIOS_MYSQL, $options));
+    $settings->add(new admin_setting_configselect('sumarios_db_type', 
+      get_string('sumarios_db_type', 'sumarios'),
+      get_string('sumarios_db_type_text', 'sumarios'), 
+      SUMARIOS_MYSQL, 
+      $options)
+      );
                                               
-		$settings->add(new admin_setting_configtext('sumarios_db_server', get_string('sumarios_db_server', 'sumarios'),
-											 get_string('sumarios_db_server_text', 'sumarios'), "" , PARAM_HOST));
+		$settings->add(new admin_setting_configtext('sumarios_db_server', 
+		  get_string('sumarios_db_server', 'sumarios'),
+			get_string('sumarios_db_server_text', 'sumarios'), 
+			"" , 
+			PARAM_HOST)
+			);
 											 
-		$settings->add(new admin_setting_configtext('sumarios_db_user', get_string('sumarios_db_user', 'sumarios'),
-											 get_string('sumarios_db_user_text', 'sumarios'), ""));
+		$settings->add(new admin_setting_configtext('sumarios_db_user', 
+		  get_string('sumarios_db_user', 'sumarios'),
+			get_string('sumarios_db_user_text', 'sumarios'), 
+			"")
+			);
 											 
-		$settings->add(new admin_setting_configpasswordunmask('sumarios_db_pass', get_string('sumarios_db_pass', 'sumarios'),
-											 get_string('sumarios_db_pass_text', 'sumarios'), ""));
+		$settings->add(new admin_setting_configpasswordunmask('sumarios_db_pass', 
+			get_string('sumarios_db_pass', 'sumarios'),
+			get_string('sumarios_db_pass_text', 
+			'sumarios'), 
+			"")
+			);
 											 
-		$settings->add(new admin_setting_configtext('sumarios_db_table', get_string('sumarios_db_table', 'sumarios'),
-											 get_string('sumarios_db_table_text', 'sumarios'), ""));
+		$settings->add(new admin_setting_configtext('sumarios_db_table', 
+		  get_string('sumarios_db_table', 'sumarios'),
+			get_string('sumarios_db_table_text', 'sumarios'), 
+			"")
+			);
 											 
-		$settings->add(new admin_setting_configtext( 'sumarios_cron' , get_string('sumarios_cron', 'sumarios'),
-											 get_string('sumarios_cron_text', 'sumarios'), $module->cron , PARAM_INT ));
+		$settings->add(new admin_setting_configtext( 'sumarios_cron' , 
+		  get_string('sumarios_cron', 'sumarios'),
+			get_string('sumarios_cron_text', 'sumarios'), 
+			$module->cron , 
+			PARAM_INT )
+			);
 											 
 }
