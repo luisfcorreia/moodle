0b71ca01edca822c1d6a6f37d0d542b5ebd61257
Luis Correia <buga@loide.net>
2012-06-02 18:16:42 +0100

    add string translation for Portuguese

---
+++ sumarios/lang/pt/sumarios.php
@@ -0,0 +1,71 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Portuguese strings for sumarios
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod
+ * @subpackage sumarios
+ * @copyright  2012 José Andrade & Luis Correia for Universidade Atlântica
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Sumários';
+$string['modulenameplural'] = 'sumários';
+$string['modulename_help'] = 'O módulo dos sumários pretende recolher e exportar a informação | que os docentes preenchem sobre os assuntos dados em aula';
+$string['sumariosfieldset'] = '';
+$string['sumariosname'] = 'Sumário da aula';
+$string['sumariosname_help'] = 'Sumário da aula';
+$string['sumarios'] = 'Sumários';
+$string['pluginadministration'] = 'Administração dos sumários';
+$string['pluginname'] = 'sumários';
+$string['sumariostexto'] = 'Texto dos sumários';
+$string['sumariostexto_help'] = 'Texto dos sumários';
+$string['sumarioscabecalho'] = 'Summary for this lesson:';
+$string['sumariosdata'] = 'Data em que a aula foi dada';
+
+$string['sumarios_db_type'] = 'Tipo do servidor externo';
+$string['sumarios_db_server'] = 'Nome ou IP do servidor';
+$string['sumarios_db_database'] = 'Base de dados';
+$string['sumarios_db_user'] = 'Utilizador';
+$string['sumarios_db_pass'] = 'Password';
+$string['sumarios_db_table'] = 'Tabela';
+$string['sumarios_cron'] = 'Minutos entre cada exportação';
+
+$string['sumarios_db_type_text'] = 'Escolha o tipo de servidor externo';
+$string['sumarios_db_server_text'] = 'Defina nome ou IP do servidor';
+$string['sumarios_db_database_text'] = 'Defina qual a base de dados';
+$string['sumarios_db_user_text'] = 'Defina utilizador';
+$string['sumarios_db_pass_text'] = 'Defina password';
+$string['sumarios_db_table_text'] = 'Defina a tabela destino para a exportação';
+$string['sumarios_cron_text'] = 'Minutos entre cada exportação';
+
+$string['sumarios_cron_00'] = 'Starting sumarios cron job';
+$string['sumarios_cron_01'] = 'Finished sumarios cron job';
+$string['sumarios_cron_02'] = 'Connect to external database failed!!!';
+$string['sumarios_cron_03'] = 'Values are not set in configuration page!';
+$string['sumarios_cron_04'] = 'Inserting record on external database';
+$string['sumarios_cron_05'] = 'DATABASE ENGINE NOT AVAILABLE! ';
+$string['sumarios_cron_06'] = 'Unknown driver ';
+$string['sumarios_cron_07'] = 'moodle_exception';
+
